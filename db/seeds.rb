88c91d576df9d2426a0155c4c4cdf85255fefda6
Xzi <nehetejay@gmail.com>
2023-09-24 12:16:35 -0500

    Added new movie

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,7 +17,10 @@ more_movies = [
     {:title => 'Nomadland', :rating => 'R',
       :release_date => '19-Feb-2021'},
     {:title => 'CODA', :rating => 'PG-13',
-      :release_date => '13-Aug-2021'}
+      :release_date => '13-Aug-2021'},
+    {:title => 'The Lord of the Rings: The Return of the King', :rating => 'PG-13',
+      :release_date => '17-Dec-2003'
+    }
   ]
   
   more_movies.each do |movie|
